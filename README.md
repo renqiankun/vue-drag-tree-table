864da4c772fbc882b9632f25a6d234502380b848
qiankun <526472759@qq.com>
2023-10-19 10:21:54 +0800

    打包

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-参考 https://github.com/mafengwo/vue-drag-tree-table
+
+
+参考自 https://github.com/mafengwo/vue-drag-tree-table
+
+git:https://renqiankun.github.io/vue-drag-tree-table
 
 预览 https://renqiankun.github.io/vue-drag-tree-table/dist/#/
+使用 npm i vue-drag-table
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'./',
   plugins: [vue()],
+  build:{
+    outDir: path.join(__dirname, 'lib'),
+    lib:{
+      entry: path.resolve(__dirname, 'src/lib/index.js'),
+      name: 'vue-drag-table',
+      fileName: 'vue-drag-table',
+    },
+    rollupOptions:{
+      external:['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    }
+  }
 })
